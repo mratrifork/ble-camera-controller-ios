296c41338f0a195951658c16ba91529d9a86ff40
Martin Rasmussen <mra@trifork.com>
2023-05-11 14:37:16 +0200

    Add initial project

---
+++ CameraController/CameraController/CameraControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraControllerApp.swift
+//  CameraController
+//
+//  Created by Martin Rasmussen on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
